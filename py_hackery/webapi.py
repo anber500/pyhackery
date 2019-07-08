87b874c4ba5e0eebac7430da8518e73ff31e30ba
micahj <micah.webb@talkingtech.com>
2019-07-09 10:50:38 +1200

    moved test folder up a level

--- py_hackery/webapi.py
+++ py_hackery/webapi.py
@@ -2,13 +2,14 @@
 
 # now see here : http://flask.pocoo.org/
 from flask import Flask
+from awesomeness.awesome import something_awesome
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello():
-    return "<h1>Hello</h1>"
+    return f"<h1>{something_awesome()}</h1>"
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port='8081')
