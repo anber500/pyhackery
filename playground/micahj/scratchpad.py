7177283151b3a58955d86043330c8000a742714a
micahj <micah.webb@talkingtech.com>
2019-07-12 15:15:36 +1200

    renamed py_hackery same as repo.. added playground work

---
+++ playground/micahj/scratchpad.py
@@ -0,0 +1,22 @@
+import sys
+import os
+import json
+from pyhackery.awesomeness.awesome import something_awesome
+
+
+def jprint(data):
+    text = json.dumps(data, indent=3)
+    print(text)
+
+
+def main(args=[]):
+    jprint(data=sys.path)
+    print(sys.path)
+    print(something_awesome())
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
+
+# from workspace root run from command line like so
+# python -m playground.micahj.scratchpad
