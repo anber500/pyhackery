ce572964d61e2f0020bab7cb2dbfe0c5e68b75da
buddike1112 <buddikea1112@gmail.com>
2019-07-30 14:01:01 +1200

    Adding sample to publish rabbitMQ message

---
+++ playground/buddike/lab3/rabbitmq_client.py
@@ -0,0 +1,22 @@
+import pika
+
+
+def publish_to_rabbitmq(message, queue):
+    print("Trying to connect to RabbitMQ instance")
+
+    with pika.BlockingConnection(pika.ConnectionParameters('localhost')) as connection:
+        print("Connected to local RabbitMQ instance")
+        channel = connection.channel()
+
+        try:
+            channel.queue_declare(queue=queue)
+            print("queue.....created")
+
+            try:
+                print("Trying to publishing simple message...")
+                channel.basic_publish(exchange='', routing_key=queue, body=message)
+                print("Test message published")
+            except:
+                print("Unable to publish the message to queue")
+        except:
+            print("Unable to create queue in local rabbit instance")
