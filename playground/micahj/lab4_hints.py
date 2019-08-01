830ff521e327f49f191d680e913a8b0a2f0c8387
micahj <micah.webb@talkingtech.com>
2019-08-01 15:54:12 +1200

    added sample usage of csv and to_lookup

--- playground/micahj/lab4_hints.py
+++ playground/micahj/lab4_hints.py
@@ -2,16 +2,29 @@ import os
 
 from playground.micahj.scratchpad import jprint
 from pyhackery.csv_hack import read_a_simple_csv
+from pyhackery.utils import to_lookup
 
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 
+# load csv file data
+data = read_a_simple_csv(path=os.path.join(CURRENT_DIR, 'customer.csv'))
+jprint(data, "data from csv file")
 
-def try_csv():
-    data = read_a_simple_csv(path=os.path.join(CURRENT_DIR, 'customer.csv'))
-    jprint(data)
+# turn the list into a lookup - group on name : just like in dotnet lambdas
+# are a quick inline way of writing a function
+# go and have a look at the to_lookup function
+lookup = to_lookup(data, key_func=lambda item: item['name'].lower())
+jprint(lookup, "converted to a lookup using inline lambda function")
 
 
-try_csv()
+def key_selector(item):
+    return item['name'].lower()
+
+
+# we can use the function 'key_selector' instead of the inline lambda if
+# you find that clearer - the result will be the same
+lookup = to_lookup(data, key_func=key_selector)
+jprint(lookup, "converted to a lookup using a function (def)")
 
 # run with
 # python -m playground.micahj.lab4_hints
--- playground/micahj/scratchpad.py
+++ playground/micahj/scratchpad.py
@@ -4,8 +4,10 @@ import json
 from pyhackery.awesomeness.awesome import something_awesome
 
 
-def jprint(data):
+def jprint(data, title=""):
     text = json.dumps(data, indent=3)
+    if title:
+        print(title)
     print(text)
 
 
